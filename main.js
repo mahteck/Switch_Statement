379d633bacbcb56984e2f2ac559013b8155ca300
mshoaib1244 <shoaib1244@gmail.com>
2024-04-29 19:58:43 +0500

    First Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var dayOff = "Sunday";
+switch (dayOff) {
+    case "Saturday":
+        console.log("go hiking");
+        break;
+    case "Sunday":
+        console.log("Read a book");
+        break;
+    default:
+        console.log("work on a hobby");
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+let dayOff = "Sunday";
+switch (dayOff) {
+    case "Saturday":
+        console.log("go hiking");
+        break;
+    case "Sunday":
+        console.log("Read a book");
+        break;
+    default:
+        console.log("work on a hobby");
+}
